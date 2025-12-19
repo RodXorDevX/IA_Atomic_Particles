cd84dc70e0efae78c8c84fe82ec83862e3685017
RodXorDevX <rodxordevx@gmail.com>
2025-12-19 04:26:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 export default {
   base: '/',
+  root: './',
+  publicDir: './',
   server: {
     port: 5173,
     strictPort: false,
+    open: '/index.html'
   },
   build: {
     target: 'esnext',
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 }
